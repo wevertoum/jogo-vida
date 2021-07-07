3e620d8bd4344e292a4c48fd7fde50a220a22c5c
Weverton Rodrigues <wevertonfr@discente.ufg.br>
2021-07-07 01:33:34 -0300

    incluindo resultado page

---
+++ src/pages/ResultadoPage/ResultadoPage.tsx
@@ -0,0 +1,33 @@
+import PageContainer from "components/PageContainer/TypingTexts";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./ResultadoPage.less";
+
+interface Props {}
+const ResultadoPage: React.FC<Props> = () => {
+  return (
+    <PageContainer>
+      <div className="instrucoes-content">
+        <div className="titulo-instrucoes">COMO JOGAR?</div>
+        <div className="conteudo-instrucoes">
+          Essa roda vai fazer você refletir sobre alguns aspectos importantes a
+          serem considerados nos comportamentos e atitudes para o engajamento do
+          paciente, bem como: identificar as potencialidades, recursos e
+          limitações que existem em você para aprimorar ou manter a realidade
+          atual.
+          <br />
+          <br />
+          Ao responder você deve selecionar a alternativa que melhor representa
+          seu comportamento e/ou atitudes atuais para o engajamento do paciente,
+          de acordo com cada situação apresentada.
+        </div>
+
+        <NavLink to="game">
+          <div className="btn-go-instrucoes">OK!</div>
+        </NavLink>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default ResultadoPage;
