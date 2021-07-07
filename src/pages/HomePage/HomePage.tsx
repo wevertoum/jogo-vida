f0475a6e12b81c15766a6918e6a5ac9d1fbd11b4
Weverton Rodrigues <wevertonfr@discente.ufg.br>
2021-07-06 22:46:31 -0300

    another try works less

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,7 @@ import "./HomePage.less";
 
 interface Props {}
 const HomePage: React.FC<Props> = () => {
-  return (
-    <>
-      <div className="main-content">a</div>
-    </>
-  );
+  return <div className="main-content">a</div>;
 };
 
 export default HomePage;
